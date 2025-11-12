d12e497b959507a8c37c4dd5fc2b49b6caa7c218
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-11-12 06:30:00 -0700

    adding main test class to try out junit jupiter

---
+++ src/TestMain.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class StandardTests {
+
+    @BeforeAll
+    static void initAll() {
+
+    }
+
+}
