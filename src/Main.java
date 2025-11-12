17188ac1f560fa28e7e884aee2cdc78a8ba783ac
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-11-12 05:31:42 -0700

    adding order of execution of calculations

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Collect illuminance data from device
+
+        // Convert to DNI
+
+        // Calculate solar potential of rooftop surface
+
+        // Determine power output of solar panel
+
+        // Determine which devices can be powered by solar panel
+
+    }
+}
