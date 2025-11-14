0c215199c551730519cf161f58bb2979c7d15f54
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-11-14 06:08:32 -0700

    playing around with Display generation for test methods and classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.time.LocalDateTime;
+
 public class Main {
+   // LocalDateTime TimeOpened;
+   // int SLA;
+
     public static void main(String[] args) {
+        // int sum1;
+        // sum1=sum(3,4);
+        // System.out.println(sum1);
+
+        // int minus;
+        // minus=difference(7, 5);
+        // System.out.println(minus);
+
+        // System.out.println(new Main());
+
+
+
         // Collect illuminance data from device
 
         // Convert to DNI
@@ -11,4 +28,21 @@ public class Main {
         // Determine which devices can be powered by solar panel
 
     }
+    // public void String (){
+    //     System.out.println("Duedatwe:" + this.SLA);
+    // }
+    // public  static int sum(int a, int b){
+    //    int addition=a+b;
+    //    return addition;
+    // }
+    // public  static int difference(int a, int b){
+    //     int subtraction=a-b;
+    //     return subtraction;
+    // }
+    // public LocalDateTime dueDate(){
+    //     LocalDateTime due=TimeOpened.plusDays(this.SLA);
+    //     return due;
+    // }
+
+
 }
