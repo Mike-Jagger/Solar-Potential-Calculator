02b042ffd8e61f1d92f10fe9c6977dd0132caee2
KingsleyNahyi <kingsleynahyi15@gmail.com>
2025-12-06 22:55:22 +0000

    Factored in orientation in calculations

--- Rooftop.java
+++ Rooftop.java
@@ -2,7 +2,16 @@ public class Rooftop {
   private double area; 
   private double shadingFactor; 
   private String orientation; 
-
+  
+public double getOrientationFactor() {
+    switch (orientation) {
+        case "south": return 1.0;  
+        case "east":  return 0.8;   
+        case "west":  return 0.8;   
+        case "north": return 0.6;   
+        default:      return 0.9;   
+    }
+}
   public Rooftop(double area, double shadingFactor, String orientation){
     this.area = area; 
     this.shadingFactor = shadingFactor; 
