b001acc2f6c0baf4b3f2ac91d627a834d397e181
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2025-12-09 13:37:22 -0700

    adjusting error loggin message to be more meaningful

--- src/PhyphoxSolarMonitor.java
+++ src/PhyphoxSolarMonitor.java
@@ -13,7 +13,8 @@ public class PhyphoxSolarMonitor {
         PhyphoxClient client = new PhyphoxClient(baseUrl);
 
         if (!establishInitialConnection(client)) {
-            System.err.println("Fatal Error: Could not establish connection after multiple attempts.");
+            System.err.println("Fatal Error: Could not establish connection after multiple attempts. " +
+                    "\n Please try running the app again and provide the right Phyphox base URL.");
             return;
         }
 
